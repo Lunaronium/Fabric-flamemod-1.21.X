368a5bea230d2abeb9a65eb73b629910f98e87f5
Tmusk <tmuskcloud@gmail.com>
2025-02-24 13:14:17 +0100

    Initial commit

---
+++ src/main/java/net/luna/flamemod/FlameMod.java
@@ -0,0 +1,16 @@
+package net.luna.flamemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlameMod implements ModInitializer {
+	public static final String MOD_ID = "flamemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/luna/flamemod/FlameModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.luna.flamemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FlameModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
